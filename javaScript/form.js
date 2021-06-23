520c5fd2143fd98a5a17a0aa072d249dd420c217
TRECN <rishabh1103@outlook.com>
2021-06-23 23:28:45 +0530

    commit

--- javaScript/form.js
+++ javaScript/form.js
@@ -2,4 +2,7 @@ function clicked(){
     var y=document.getElementById('fname');
     var x=document.getElementById('print');
     x.innerHTML="name: "+y.value;
+    var y=document.getElementById('froll');
+    var x=document.getElementById('print2');
+    x.innerHTML="roll: "+y.value;
 }
