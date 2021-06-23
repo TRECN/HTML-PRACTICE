3b6fe1191ebbbc1cc37950f8f0b8056298d2d91f
TRECN <rishabh1103@outlook.com>
2021-06-24 00:11:28 +0530

    commit

---
+++ javaScript/form-select.js
@@ -0,0 +1,6 @@
+function c(){
+    var e=document.getElementById('place');
+    var x=document.getElementById('print3');
+    var text= e.value;
+    x.innerHTML=text;
+}
--- javaScript/form.js
+++ javaScript/form.js
@@ -6,9 +6,3 @@ function clicked(){
     var x=document.getElementById('print2');
     x.innerHTML="roll: "+y.value;
 }
-function clickedd(){
-    var y=document.getElementsById('place');
-    var x=document.getElementById('print3');
-    var text=y.options[y.selectedIndex].text;
-    x.innerHTML=text;
-}
